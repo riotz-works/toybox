cce3c3024c8efdcce42284b17a4a52a0a3cb0894
lulzneko <lulzneko@riotz.works>
2026-04-23 18:18:39 +0900

    build: configure basic settings for @stylistic/eslint-plugin

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import globals from 'globals';
 import js from '@eslint/js';
 import pkg from './package.json' with { type: 'json', };
 import { readFileSync, } from 'node:fs';
+import stylistic from '@stylistic/eslint-plugin';
 import { configs as tsConfigs, } from 'typescript-eslint';
 import tsPlugin from '@typescript-eslint/eslint-plugin';
 
@@ -12,6 +13,7 @@ export default [
   { settings: { node: { version: pkg.engines.node, }, }, },
   { ignores: [ ...readFileSync('.gitignore', 'utf-8',).split('\n',).filter((line,) => line && !line.startsWith('#',),), ], },
   js.configs.all,
+  stylistic.configs.all,
 
   ...tsConfigs.all,
   {
@@ -36,6 +38,7 @@ export default [
       '@typescript-eslint/no-use-before-define': 'off', // To keep readability by declaring lower importance(like private function) at low position in file.
 
       // Customize Lint settings
+      '@stylistic/newline-per-chained-call':            [ 'error', { ignoreChainWithDepth: 5, },], // To take advantage of chained call flexibility.
       '@typescript-eslint/consistent-type-definitions': [ 'error', 'type', ],                      // Use type alias instead of interface to prevent dangerous type overrides
 
       // Custom rule: Force "right side is larger" inequality
@@ -69,6 +72,25 @@ export default [
       'no-nested-ternary':                                  [ 'off', ],
       'no-ternary':                                         [ 'off', ],
       'one-var':                                            [ 'error', 'never', ],
+      '@stylistic/array-bracket-newline':                   [ 'error', 'consistent', ],
+      '@stylistic/array-bracket-spacing':                   [ 'error', 'always', { arraysInArrays: false, objectsInArrays: false, },],
+      '@stylistic/array-element-newline':                   [ 'error', 'consistent', ],
+      '@stylistic/brace-style':                             [ 'error', '1tbs', { allowSingleLine: true, },],
+      '@stylistic/comma-dangle':                            [ 'error', 'always', ],
+      '@stylistic/function-call-argument-newline':          [ 'error', 'consistent', ],
+      '@stylistic/indent':                                  [ 'error', 2, { SwitchCase: 1, },],
+      '@stylistic/key-spacing':                             [ 'error', { mode: 'minimum', },],
+      '@stylistic/lines-between-class-members':             [ 'error', 'always', { exceptAfterSingleLine: true, },],
+      '@stylistic/multiline-comment-style':                 [ 'error', 'separate-lines', ],
+      '@stylistic/multiline-ternary':                       [ 'off', ],
+      '@stylistic/no-confusing-arrow':                      [ 'off', ],
+      '@stylistic/no-multi-spaces':                         [ 'error', { exceptions: { Property: true, VariableDeclarator: true, }, ignoreEOLComments: true, },],
+      '@stylistic/object-curly-spacing':                    [ 'error', 'always', { arraysInObjects: false, objectsInObjects: false, },],
+      '@stylistic/object-property-newline':                 [ 'error', { allowAllPropertiesOnSameLine: true, },],
+      '@stylistic/padded-blocks':                           [ 'error', { switches: 'never', }, { allowSingleLineBlocks: true, },],
+      '@stylistic/quote-props':                             [ 'error', 'consistent-as-needed', ],
+      '@stylistic/quotes':                                  [ 'error', 'single', { avoidEscape: true, },],
+      '@stylistic/space-before-function-paren':             [ 'error', { anonymous: 'always', named: 'never', asyncArrow: 'always', },],
       '@typescript-eslint/max-params':                      [ 'error', { max: 5, },],
       '@typescript-eslint/prefer-readonly-parameter-types': [ 'off', ],
       '@typescript-eslint/strict-boolean-expressions':      [ 'error', { allowNullableBoolean: true, allowNullableNumber: true, allowNullableString: true, allowNullableObject: true, },],
